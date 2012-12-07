530fa23c7cde904ff048baf537d88ba2e8be1af0
Andy Hausmann <hi@andy-hausmann.de>
2012-12-07 12:28:11 +0100

    Refactoring: Changed some function calls due to refactoring.

--- app/code/community/SotaStudio/NewsletterUnsubscribe/Model/Unsubscribe.php
+++ app/code/community/SotaStudio/NewsletterUnsubscribe/Model/Unsubscribe.php
@@ -1,59 +1,59 @@
 <?php
 
-class SotaStudio_NewsletterUnsubscribe_Model_Unsubscriber extends Mage_Newsletter_Model_Subscriber
+class SotaStudio_NewsletterUnsubscribe_Model_Unsubscribe extends Mage_Newsletter_Model_Subscriber
 {
-    /**
+	/**
 	 * @var bool $_sendConfirmationSuccessEmail check if email already send
 	 */
-    private $_sendConfirmationSuccessEmail = true;
+	private $_sendConfirmationSuccessEmail = true;
 
 
-    /**
-     * unsubscribes by email
-     *
-     * @param string $email
-     * @throws Exception
-     * @return int
-     */
-    public function unsubscribeByEmail($email) {
-        $this->setStoreId(Mage::app()->getStore()->getId());
-        $this->loadByEmail($email);
-        if( !$this->getId() ) {
-            return false;
-        }
-        $this->setCheckCode($this->getCode());
-        $this->unsubscribe();
-        return $this->getStatus();
-    }
-    
-    /**
-     * Confirms subscriber newsletter
-     *
-     * @param string $code
-     * @return boolean
-     */
-    public function confirm($code)
-    {
-        $parent = (bool) parent::confirm($code);
-        if( $parent ) {
-            $this->sendConfirmationSuccessEmail();
-            return true;
-        }
-        return false;
-    }
+	/**
+	 * unsubscribes by email
+	 *
+	 * @param string $email
+	 * @throws Exception
+	 * @return int
+	 */
+	public function unsubscribeByEmail($email) {
+		$this->setStoreId(Mage::app()->getStore()->getId());
+		$this->loadByEmail($email);
+		if( !$this->getId() ) {
+			return false;
+		}
+		$this->setCheckCode($this->getCode());
+		$this->unsubscribe();
+		return $this->getStatus();
+	}
 
-    /**
-     * Sends out confirmation success email
-     * 
-     * @see Mage_Newsletter_Model_Subscriber::sendConfirmationSuccessEmail
-     * @return Mage_Newsletter_Model_Subscriber
-     */
-    public function sendConfirmationSuccessEmail() {
-        // do not send two E-Mails, maybe Magento will implement this line in method self::confirm($code)
-        if( $this->_sendConfirmationSuccessEmail ) {
-            parent::sendConfirmationSuccessEmail();
-            $this->_sendConfirmationSuccessEmail = !$this->_sendConfirmationSuccessEmail;
-        }
-        return $this;
-    }
+	/**
+	 * Confirms subscriber newsletter
+	 *
+	 * @param string $code
+	 * @return boolean
+	 */
+	public function confirm($code)
+	{
+		$parent = (bool) parent::confirm($code);
+		if( $parent ) {
+			$this->sendConfirmationSuccessEmail();
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Sends out confirmation success email
+	 *
+	 * @see Mage_Newsletter_Model_Subscriber::sendConfirmationSuccessEmail
+	 * @return Mage_Newsletter_Model_Subscriber
+	 */
+	public function sendConfirmationSuccessEmail() {
+		// do not send two E-Mails, maybe Magento will implement this line in method self::confirm($code)
+		if( $this->_sendConfirmationSuccessEmail ) {
+			parent::sendConfirmationSuccessEmail();
+			$this->_sendConfirmationSuccessEmail = !$this->_sendConfirmationSuccessEmail;
+		}
+		return $this;
+	}
 }
